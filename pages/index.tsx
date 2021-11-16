beb67e0b9aec60bf7642db6571b4c47f82833697
Bruno Noriller <brunonoriller@gmail.com>
2021-11-16 14:27:43 -0300

    fix: :lipstick: fixing mobile layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from '@chakra-ui/layout';
+import { Flex } from '@chakra-ui/layout';
 import type { NextPage } from 'next';
 import React from 'react';
 import { VideoContainer } from '../src/main/VideoContainer';
@@ -7,7 +7,10 @@ import { Header } from '../src/shared/Header';
 import { MetaHead } from '../src/shared/MetaHead';
 
 const Home: NextPage = () => (
-  <Flex flexDirection='column' minH='100vh'>
+  <Flex
+    flexDirection='column'
+    minH='100%'
+    position='fixed'>
     <MetaHead title='Bruno Noriller - Bandersnatch Interview' />
 
     <Header />
